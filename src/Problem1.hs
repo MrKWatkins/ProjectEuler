c9e613e63eb9aeecf2d45843bbadbfcbb2e5f979
Kevin Watkins <mrkwatkins@gmail.com>
2013-10-05 20:10:32 +0100

    Solved problem 1

---
+++ src/Problem1.hs
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+--
+-- Multiples of 3 and 5
+--
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5,
+-- we get 3, 5, 6 and 9. The sum of these multiples is 23.
+--
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+--
+-----------------------------------------------------------------------------
+
+module Problem1 (
+    problem1
+) where
+
+problem1 :: Integer -> Integer
+problem1 maxExclusive = sum [ x | x <- [1..(maxExclusive-1)], x `mod` 3 == 0 || x `mod` 5 == 0]
+
