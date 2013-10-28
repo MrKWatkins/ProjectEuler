8570733240d07c4e41b685fc08fe57407b34fcdd
Kevin Watkins <mrkwatkins@gmail.com>
2013-10-28 21:28:13 +0000

    Added timing to Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -10,7 +10,20 @@ module Main (
     main
 ) where
 
+import Text.Printf
+import Control.Exception
+import System.CPUTime
+
 import Problem14
 
+-- Timing based on http://www.haskell.org/haskellwiki/Timing_computations.
+
 main = do
+    startedAt <- getCPUTime
+    putStr "Answer: "
+
     putStrLn $ show problem14
+
+    finishedAt <- getCPUTime
+    let diff = (fromIntegral (finishedAt - startedAt)) / (10^12)
+    printf "Time taken: %0.3fs" (diff :: Double)
