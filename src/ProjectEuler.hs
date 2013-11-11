7fcbce5e16610271ed8778f5c77fda978d1b22cb
Kevin Watkins <mrkwatkins@gmail.com>
2013-11-11 22:38:13 +0000

    Solved problem 17

--- src/ProjectEuler.hs
+++ src/ProjectEuler.hs
@@ -14,7 +14,7 @@ import Text.Printf
 import Control.Exception
 import System.CPUTime
 
-import Problem21
+import Problem17
 
 -- Timing based on http://www.haskell.org/haskellwiki/Timing_computations.
 
@@ -22,7 +22,7 @@ main = do
     startedAt <- getCPUTime
     putStr "Answer: "
 
-    print problem21
+    print problem17
 
     finishedAt <- getCPUTime
     let diff = fromIntegral (finishedAt - startedAt) / (10^12)
